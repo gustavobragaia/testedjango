b7b27fbcf11b0a0470b46e4ef22db8632524acf2
GBS <gustavobragaia12@gmail.com>
2024-11-18 09:39:26 -0300

    Ajuste no arquivo settings.py para corrigir erro de configuração.

--- testeDjango/settings.py
+++ testeDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5)xqjj@=mf!nc2snzic_=*t-lo8x-&mdd4zmit!0v2i_juhfy2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["137.184.220.12"]
 
